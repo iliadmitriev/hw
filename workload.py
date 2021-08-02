8b357757f7c499f4a259c26df8acd5a37f9c7241
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-08-02 16:12:56 +0300

    Create workload.py

---
+++ workload.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    x = 0.0
+    for j in range(1, 1_000_000):
+        x += sqrt(j)
+    return 'Sum of sqrt(x), x between 1 and 1 million is {}!\n'.format(x)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
